158fbd2d4a0c4665ae16a91d9a00d45a67ac582a
“yyviolin52git config --global user.emall “yyviolin52@gmail.com <yyviolin52@gmail.com>
2020-06-17 22:17:43 -0500

    Site updated: 2020-06-17 22:17:43

---
+++ js/copyright.js
@@ -0,0 +1,1 @@
+!function(t,s,e){var o,c=document.currentScript||(o=document.getElementsByTagName("script"))[o.length-1],n=$(c).attr("successtext");new ClipboardJS(".fa-clipboard").on("success",function(){n&&(toastr.options={positionClass:"toast-top-center",timeOut:"1000"},toastr.success(n))})}(window,document);
